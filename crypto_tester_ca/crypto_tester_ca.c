1c57f175735cec9ea6e581dd5fafbeb07b5009b2
Hannu1 <hannu.lahtinen@student.tut.fi>
2014-12-14 19:29:08 +0200

    Comments and better error handling
    Change-Id: I813eb58384ab513358b83182d18048e0c35d6574
    
    changed the copyright information at the start of the file.
    
    Change-Id: I265f774d3bb34f4fc72627f1106ee16a293b60d4
    Signed-off-by: Hannu1 <hannu.lahtinen@student.tut.fi>

--- crypto_tester_ca/crypto_tester_ca.c
+++ crypto_tester_ca/crypto_tester_ca.c
@@ -1,5 +1,5 @@
 /*****************************************************************************
-** Copyright (C) 2014 Intel Corporation.                                    **
+** Copyright 2014 Hannu Lahtinen hannu.lahtinen@student.tut.fi              **
 **                                                                          **
 ** Licensed under the Apache License, Version 2.0 (the "License");          **
 ** you may not use this file except in compliance with the License.         **
@@ -12,6 +12,16 @@
 ** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. **
 ** See the License for the specific language governing permissions and      **
 ** limitations under the License.                                           **
+**                                                                          **
+** ClientApplication for Open-TEE project that is indended                  **
+** to test handling of SHA calculations through out the project             **
+**                                                                          **
+** This file was created as part of an assignment work                      **
+** for course "Turvallinen ohjelmointi part 2" in                           **
+** Tampere University of Technology                                         **
+**                                                                          **
+** NOTE: This application is based on example_sha1_ca found at:             **
+** http://github.com/Open-TEE/CAs/tree/master/example_sha1_ca               **
 *****************************************************************************/
 
 #include "tee_client_api.h"
@@ -25,7 +35,18 @@ static const TEEC_UUID uuid = {
 	0x12345678, 0x8765, 0x4321, { 'S', 'H', 'A', 'T', 'E', 'S', 'T', 'S' }
 };
 
-#define MAX_ARGUMENT_LENGTH 50
+#define MAX_ARGUMENT_LENGTH 100
+
+/*
+ * Defines the max amount of characters in one input length read from the file.
+ */
+#define MAX_INPUT_CHAR_LENGTH 10
+
+/*
+ * Defines how long a string has to be before it is split into multiple updates
+ * NOTE: needs to be less than DATA_SIZE to prevent buffer overflow on CA side
+ */
+#define MAX_INPUT_LENGTH_SINGLE_OPERATION 100
 
 /* Data buffer sizes */
 #define DATA_SIZE	512
@@ -47,9 +68,6 @@ static const TEEC_UUID uuid = {
 #define HASH_SHA384	0x00000004
 #define HASH_SHA512	0x00000005
 
-#define MAX_INPUT_CHAR_LENGTH 10
-#define MAX_INPUT_LENGTH_SINGLE_OPERATION 100
-
 /* error codes */
 #define UNEXPECTED_RESULT_FROM_TEST 1
 #define UNKNOWN_ERROR 2
@@ -63,7 +81,12 @@ static const TEEC_UUID uuid = {
 #define FAILED_TO_REGISTER_INPUT_MEMORY 10
 #define FAILED_TO_REGISTER_OUTPUT_MEMORY 11
 #define FAILED_TO_INVOKE_COMMAND 12
-#define ZERO_LENGTH_INPUTS 13
+#define OUTPUT_FILE_ENDED_UNEXPECTEDLY 13
+
+/*
+ * Turns a string in to an int
+ * String can contain line end characters
+ */
 uint32_t get_input_length(char* input_length_str) {
 	uint32_t j;
 	char *error;
@@ -79,6 +102,14 @@ uint32_t get_input_length(char* input_length_str) {
 	return (uint32_t)length;
 }
 
+/*
+ * Feeds x bytes from input_file to TA for every line in length
+ * where x is the number value on that line in length file
+ *
+ * Compares output from TA to a line in expected_output_file
+ * Line number of compared line is the same as line number
+ * read from length file
+ */
 int run_sha_tests(char *input_file, char *length_file,
 				char *expected_output_file, char *algorithm_to_test)
 {
@@ -121,6 +152,7 @@ int run_sha_tests(char *input_file, char *length_file,
 		algorithm = HASH_SHA512;
 		size_to_use = SHA512_SIZE;
 	} else {
+		printf("Unknown algorithm: %s\n", algorithm_to_test);
 		return UNKNOWN_ALGORITHM_ERROR;
 	}
 
@@ -180,30 +212,35 @@ int run_sha_tests(char *input_file, char *length_file,
 	/* open files before loop */
 	input_fd = fopen(input_file, "rb");
 	if (input_fd == NULL) {
+		printf("Failed to open input file\n");
 		return_code = INVALID_INPUT_FILE;
 		goto end_4;
 	}
 
 	length_fd = fopen(length_file, "r");
 	if (length_fd == NULL) {
+		printf("Failed to open length file\n");
 		return_code = INVALID_LENGTH_FILE;
 		goto end_5;
 	}
 	output_fd = fopen(expected_output_file, "r");
 	if (output_fd  == NULL) {
+		printf("Failed to open expected output file\n");
 		return_code = INVALID_OUTPUT_FILE;
 		goto end_5;
 	}
 
+	/* read length_fd line by line */
 	while (fgets(input_length_as_char, MAX_INPUT_CHAR_LENGTH, length_fd) != NULL) {
 		input_length = get_input_length(input_length_as_char);
-/*		if (input_length == 0) {
-			return_code = ZERO_LENGTH_INPUTS;
-			goto end_5;
-		}*/
 		++inputs;
+
+		/* update TA for every MAX_INPUT_LENGTH_SINGLE_OPERATION in input_length */
 		while (input_length > MAX_INPUT_LENGTH_SINGLE_OPERATION) {
+
+			/* read MAX_INPUT_LENGTH_SINGLE_OPERATION bytes from input file */
 			fread(input, MAX_INPUT_LENGTH_SINGLE_OPERATION, 1, input_fd);
+
 			/* Fill operation parameters */
 			operation.paramTypes = TEEC_PARAM_TYPES(TEEC_MEMREF_WHOLE,
 													TEEC_VALUE_INPUT,
@@ -211,7 +248,7 @@ int run_sha_tests(char *input_file, char *length_file,
 			operation.params[0].memref.parent = &in_mem;
 			operation.params[1].value.a = MAX_INPUT_LENGTH_SINGLE_OPERATION;
 
-			/* Invoke command */
+			/* Invoke update command */
 			ret = TEEC_InvokeCommand(&session, HASH_UPDATE, &operation, &return_origin);
 			if (ret != TEEC_SUCCESS) {
 				printf("TEEC_InvokeCommand failed: 0x%x\n", ret);
@@ -221,6 +258,9 @@ int run_sha_tests(char *input_file, char *length_file,
 				input_length -= MAX_INPUT_LENGTH_SINGLE_OPERATION;
 			}
 		}
+		/* read the rest of the input bytes
+		 * if input was empty string(ie input_length=0)
+		 * we don't need to read any bytes */
 		if (input_length > 0)
 			fread(input, input_length, 1, input_fd);
 
@@ -233,24 +273,29 @@ int run_sha_tests(char *input_file, char *length_file,
 		operation.params[1].value.a = input_length;
 		operation.params[2].memref.parent = &out_mem;
 
-		/* Invoke command */
+		/* Invoke do final command */
 		ret = TEEC_InvokeCommand(&session, HASH_DO_FINAL, &operation, &return_origin);
 		if (ret != TEEC_SUCCESS) {
 			printf("TEEC_InvokeCommand failed: 0x%x\n", ret);
 			return_code = FAILED_TO_INVOKE_COMMAND;
 			goto end_5;
 		}
-		/* Printf sha buf */
+		/* Save calculated sha buf as hex */
 		printf("Calculated %s: ", algorithm_to_test);
 		for (i = 0; i < size_to_use; i++) {
 			sprintf(&hex_output[i*2], "%02x", output[i]);
 			if (hex_output[i*2] == '\0')
 				break;
 		}
+
 		printf("%s\n", hex_output);
+
+		/* read line from expected output file */
 		if (fgets(expected_hex_output, HEX_OUTPUT_MAX_SIZE, output_fd) != NULL) {
 			expected_hex_output[strlen(expected_hex_output)-1] = '\0';
 			printf("Expected %s: %s ", algorithm_to_test, expected_hex_output);
+
+			/* Compare test result and expected result */
 			if (strcmp(expected_hex_output, hex_output) == 0) {
 				printf("Test Success!\n");
 				++succesful;
@@ -258,11 +303,18 @@ int run_sha_tests(char *input_file, char *length_file,
 				printf("Test failure!\n");
 				return_code = UNEXPECTED_RESULT_FROM_TEST;
 			}
+		} else {
+			printf("Expected output file %s", expected_output_file);
+			printf(" ended unexpectedly\n");
+			return_code = OUTPUT_FILE_ENDED_UNEXPECTEDLY;
+			break;
 		}
 	}
-	/* Cleanup used connection/resources */
+
+/* Cleanup used connection/resources */
 end_5:
-	printf("Succesfully calculated/Inputs tested: %i / %i\n", succesful, inputs);
+	printf("Results for %s:\n", input_file);
+	printf("%i / %i succesful/tested\n", succesful, inputs);
 	if (input_fd != NULL)
 		fclose(input_fd);
 	if (output_fd != NULL)
@@ -290,16 +342,13 @@ int main(int argc, char **argv)
 	char *expected_output_file;
 	char *algorithm_to_test;
 	while ((option = getopt(argc,argv, "i:l:e:a:")) != -1) {
-		if (optarg == NULL || strlen(optarg) > MAX_ARGUMENT_LENGTH || optind > argc) {
-			printf("Empty argument or too long argument(max 50 char)");
-			return ARGUMENTS_ERROR;
-		}
 		switch (option) {
 		case 'i':
 			input_file = optarg;
 			break;
 		case 'l':
 			length_file = optarg;
+			break;
 		case 'e':
 			expected_output_file = optarg;
 			break;
@@ -307,15 +356,21 @@ int main(int argc, char **argv)
 			algorithm_to_test = optarg;
 			break;
 		case '?':
-			printf("Unknown argument option");
+			printf("Unknown argument option\n");
 			return ARGUMENTS_ERROR;
 		case ':':
-			printf("Missing argument");
+			printf("Missing argument\n");
 			return ARGUMENTS_ERROR;
 		default:
-			printf("Unknown arguments error");
+			printf("Unknown arguments error\n");
 			return ARGUMENTS_ERROR;
 		}
+	}
+	if (input_file == NULL || expected_output_file == NULL
+			|| length_file == NULL || algorithm_to_test == NULL) {
+		printf("All arguments are mandatory!\n");
+		printf("Usage: ./this -aSHA-1 -iinput.in -eexpected.out -linput.length\n");
+		return ARGUMENTS_ERROR;
 	}
 	printf("input file: %s\n", input_file);
 
