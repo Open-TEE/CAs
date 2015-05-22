1fd9411713b44a6006a72ef09c6a0ade95f05db1
Ville Kankainen <villex.kankainen@intel.com>
2015-05-22 17:32:12 +0300

    storage test android update
    Change-Id: I69e6a51cea0ffbca8d62569031efdd840564ce43
    Signed-off-by: Ville Kankainen <villex.kankainen@intel.com>

---
+++ storage_test_ca/Android.mk
@@ -0,0 +1,22 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE        :=  storage_test_app
+LOCAL_SRC_FILES	    :=   \
+				storage_test_ca.c
+
+LOCAL_C_FLAGS       :=  -rdynamic -DANDROID
+
+LOCAL_C_INCLUDES    :=  \
+
+
+LOCAL_SHARED_LIBRARIES := libdl libtee
+
+ifeq ($(TARGET_ARCH),arm)
+LOCAL_LDFLAGS := -Wl,--hash-style=sysv
+endif
+
+include $(BUILD_EXECUTABLE)
+
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/include
